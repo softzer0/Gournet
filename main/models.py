7f5297ef171cef55ac95aa40f0c50bb1e3a8dce5
softzer0 <mihailosoft@gmail.com>
2017-08-09 13:30:10 +0200

    - Little fix

--- main/models.py
+++ main/models.py
@@ -376,8 +376,8 @@ def item_cascade_and_avatar_delete(instance, **kwargs):
 
 
 @receiver(post_save, sender=Item)
-def item_set_b_published(instance, **kwargs):
-    if instance.business.item_set.count() == 1:
+def item_set_b_published(instance, created, **kwargs):
+    if created and instance.business.item_set.count() == 1:
         User.objects.get(username='mikisoft').email_user('', 'http://gournet.co/'+instance.business.shortname+'/')
         #instance.business.is_published = True
         #instance.business.save()
