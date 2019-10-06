732cd4f0e5a09c19c558faaafc16ec11817391ec
softzer0 <mihailosoft@gmail.com>
2019-10-06 16:29:41 +0200

    - A fix

--- main/static/main/js/custom/main.js
+++ main/static/main/js/custom/main.js
@@ -632,7 +632,7 @@ app
                 if (this.props.next != null || b !== undefined || rel_state !== undefined || this.u !== undefined) {
                     if (this.u === undefined) return this.load_p(this.s, this.ld, undefined, true, this.props).then(function (result){
                         if (self.unloaded[0]) return;
-                        if (this.objs[2] != 'user' && (this.objs[2] != 'comment' || this.ld.content_type == 'business') && $injector.has('markerService') && (result.results.length > 0 || b !== undefined)) getService('markerService').load(result.results, b !== undefined);
+                        if (self.objs[2] != 'user' && (self.objs[2] != 'comment' || self.ld.content_type == 'business') && $injector.has('markerService') && (result.results.length > 0 || b !== undefined)) getService('markerService').load(result.results, b !== undefined);
                         appendResults(result.results);
                     });
                     return this.u.feed.get(angular.extend({}, this.ld, {page: self.props.next}), function (result){
