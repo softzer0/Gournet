176e68d36b354f16bf9e5cf36073560e683c2f23
softzer0 <root@gournet.co>
2020-05-05 01:23:06 +0300

    - Image uploads separated from production also

--- gournet/settings.py
+++ gournet/settings.py
@@ -283,7 +283,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media' if not DEBUG else 'media_test')
 
 # SMTP
 
